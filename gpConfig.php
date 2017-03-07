c0f21ff3e8933a01e7176d13591f18783275595b
maksim <smstepani4@gmail.com>
2017-03-07 21:36:26 +0200

    first commit

---
+++ gpConfig.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+//Include Google client library 
+include_once 'src/Google_Client.php';
+include_once 'src/contrib/Google_Oauth2Service.php';
+
+/*
+ * Configuration and setup Google API
+ */
+$clientId = 'Google client ID'; 
+$clientSecret = 'Google client secret'; 
+$redirectURL = 'Callback URL'; 
+
+
+//Call Google API
+$gClient = new Google_Client();
+$gClient->setApplicationName('Login to CodexWorld.com');
+$gClient->setClientId($clientId);
+$gClient->setClientSecret($clientSecret);
+$gClient->setRedirectUri($redirectURL);
+
+$google_oauthV2 = new Google_Oauth2Service($gClient);
+?>
